09d7e73994ed46a8ef508d4fca5f8c18010be1e1
Max Lebedev <maxlebedev@live.com>
2022-07-07 16:19:56 -0400

    up to 14 tests per gun

--- test_ttk_sim.py
+++ test_ttk_sim.py
@@ -4,6 +4,7 @@ import ttk_sim
 import example_weapons
 
 # Because we are using seeded random values, the order of the tests matters
+# To add one more test to a weapon, populate the next ttk value, and recompute all following tests
 
 random.seed(1)
 ttk_sim.headshot_chance = 60
@@ -18,24 +19,27 @@ def check_ttk(weapon, expected_ttks):
         assert almost_equal(test_ttk, expected_ttk)
 
 def test_sturm():
-    check_ttk(example_weapons.sturm, [1.5, 0.5, 0, 0, 1.5])
+    ttks = [1.5, 0.5, 0.0, 0.0, 1.5, 2.0, 0.0, 1.5, 1.5, 1.0, 1.0, 1.5, 2.0, 0.5]
+    check_ttk(example_weapons.sturm, ttks)
 
 def test_drang():
-    check_ttk(example_weapons.drang, [1.2, 1.0, 1.0, 0.6, 0.8])
+    ttks = [0.0, 1.0, 1.0, 0.8, 0.0, 0.8, 0.2, 0.0, 0.8, 0.8, 0.4, 1.0, 1.0, 1.2]
+    check_ttk(example_weapons.drang, ttks)
 
 def test_pom():
-    ttks = [1.666, 1.466, 1.733, 0.0666, 2.066]
+    ttks = [0.33, 0.13, 1.4, 0.0, 0.4, 1.0, 1.33, 1.4, 1.4, 1.4, 2.07, 1.33, 0.67, 1.33]
     check_ttk(example_weapons.piece_of_mind, ttks)
 
 def test_dmt():
-    ttks = [1.0, 0.5, 0, 1.0, 0]
+    ttks = [1.0, 0.5, 1.0, 1.0, 1.5, 0.5, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 2.0, 1.0]
     check_ttk(example_weapons.dmt, ttks)
 
 def test_crimson():
-    ttks = [1.933, 1.299,1.399,1.733,1.299]
+    ttks = [1.3, 1.3, 1.3, 1.3, 1.3, 1.07, 0.0, 0.43, 1.4, 0.53, 0.87, 1.3, 1.5, 1.3]
     check_ttk(example_weapons.crimson, ttks)
 
 if __name__ == "__main__":
+    # gen_ttks = lambda x: [round(ttk_sim.gunfight(x), 2) for _ in range(14)]
     test_sturm()
     test_drang()
     test_pom()
