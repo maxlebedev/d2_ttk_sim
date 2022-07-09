142553e413b2b7f08748c95aba0c14c98cd8573e
Max Lebedev <maxlebedev@live.com>
2022-07-09 12:32:52 -0400

    fmt

--- test_ttk_sim.py
+++ test_ttk_sim.py
@@ -8,39 +8,49 @@ import example_weapons as ew
 ttk_sim.headshot_chance = 60
 ttk_sim.bodyshot_chance = 30
 
+
 def load_test_data():
-    with open('expected_ttks.yaml', 'r') as tests:
+    with open("expected_ttks.yaml", "r") as tests:
         return SimpleNamespace(**yaml.safe_load(tests))
+
 
 td = load_test_data()
 
+
 def almost_equal(test, actual):
     return abs(test - actual) < 0.01
+
 
 def check_ttk(weapon, expected_ttks):
     for expected_ttk in expected_ttks:
         test_ttk = ttk_sim.gunfight(weapon)
         assert almost_equal(test_ttk, expected_ttk)
 
+
 def test_sturm():
     random.seed(1)
     check_ttk(ew.sturm, td.sturm)
+
 
 def test_drang():
     random.seed(1)
     check_ttk(ew.drang, td.drang)
 
+
 def test_pom():
     random.seed(1)
     check_ttk(ew.piece_of_mind, td.piece_of_mind)
+
 
 def test_dmt():
     random.seed(1)
     check_ttk(ew.dmt, td.dmt)
 
+
 def test_crimson():
     random.seed(1)
     check_ttk(ew.crimson, td.crimson)
+
 
 def generate_test():
     for weapon in [ew.piece_of_mind, ew.sturm, ew.drang, ew.dmt, ew.crimson]:
@@ -50,6 +60,7 @@ def generate_test():
             ttk = ttk_sim.gunfight(weapon)
             pt.append(round(ttk, 2))
         print(f"{weapon.name}: {pt}")
+
 
 if __name__ == "__main__":
     # gen_ttks = lambda x: [round(ttk_sim.gunfight(x), 2) for _ in range(14)]
